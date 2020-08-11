4d3ef1124dc8f81985846d8b75638f1a0b41b3b0
unknown <you@example.com>
2020-08-11 11:55:01 +0700

    test commit2

--- Article.php
+++ Article.php
@@ -17,7 +17,7 @@ class Article extends MX_Controller {
 	}
 	public function addArticle()
 	{	
-// 		text222333
+// 		aon
 		$this->load->view('add_article_view',$this->PAGE);
 	}
 	public function editArticle($article_id)
