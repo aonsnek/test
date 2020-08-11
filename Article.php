8257261c5963b535a489cc2fed26dccada958dcb
aonsnek <60951783+aonsnek@users.noreply.github.com>
2020-08-11 11:05:24 +0700

    Update Article.php

--- Article.php
+++ Article.php
@@ -17,6 +17,7 @@ class Article extends MX_Controller {
 	}
 	public function addArticle()
 	{	
+// 		text
 		$this->load->view('add_article_view',$this->PAGE);
 	}
 	public function editArticle($article_id)
